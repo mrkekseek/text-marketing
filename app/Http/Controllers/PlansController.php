ddfd901ff62ff5cdff1712756838b29db125cc76
Ievgenii Domanskyy <jb@div-art.com>
2017-11-20 14:35:44 +0200

    add stripe

--- app/Http/Controllers/PlansController.php
+++ app/Http/Controllers/PlansController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Plan;
+use Cartalyst\Stripe\Stripe;
 use Illuminate\Http\Request;
 
 class PlansController extends Controller
 {
     public function all()
 	{
+		$stripe = new Stripe();
+		$plans = $stripe->plans()->all();
 		return Plan::all();
 	}
 }
