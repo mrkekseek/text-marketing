0e67e2ab05e895c5ef7b3d0c4f3c07f5b9e32757
Ievgenii Domanskyi <id@div-art.com>
2017-12-27 17:32:03 +0200

    HA

--- app/Mail/ActivateHa.php
+++ app/Mail/ActivateHa.php
@@ -12,15 +12,19 @@ class ActivateHa extends Mailable
     use Queueable, SerializesModels;
 
     public $user;
+    public $ha;
+    public $link;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($user)
+    public function __construct($user, $ha, $link)
     {
         $this->user = $user;
+        $this->ha = $ha;
+        $this->link = $link;
     }
 
     /**
@@ -30,6 +34,10 @@ class ActivateHa extends Mailable
      */
     public function build()
     {
-        return $this->markdown('emails.activate_ha_for_admin')->subject('Activation HomeAdvisor')->with(['user' => $this->user]);
+        return $this->markdown('emails.activate_ha_for_admin')->subject('Activation HomeAdvisor')->with([
+            'user' => $this->user,
+            'ha' => $this->ha,
+            'link' => $this->link,
+        ]);
     }
 }
--- resources/views/emails/activate_ha_for_admin.blade.php
+++ resources/views/emails/activate_ha_for_admin.blade.php
@@ -1,11 +1,16 @@
 @component('mail::message')
 <p>
-	User {{ $user['firstname'] }} wants to activate HomeAdvisor.
+	User {{ $user->firstname }} wants to activate HomeAdvisor.
 </p>
 <p>
-	<b>Link for HomeAdvisor:</b><span> {{ $user['link'] }}</span>
+	<b>Link for HomeAdvisor:</b><span> {{ $link->url }}</span>
 </p>
 <p>
-	<b>Success String:</b><span> {{ $user['success'] }}</span>
+	<b>Success String:</b><span> {{ $link->success }}</span>
 </p>
+@if ( ! empty($ha->rep))
+<p>
+	<b>HomeAdvisor Rep:</b><span> {{ $ha->rep }}</span>
+</p>
+@endif
 @endcomponent
