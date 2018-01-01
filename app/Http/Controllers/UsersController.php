5047874ee3cfe5efeae2a2fe2156116ab60450fe
Ievgenii Domanskyy <jb@div-art.com>
2018-01-01 21:34:58 +0200

    commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Team;
+use App\Homeadvisor;
+use App\Survey;
+use App\Client;
+use App\Dialog;
+use App\Text;
+use App\Message;
+use App\Seance;
+use App\Review;
+use App\Answer;
+use App\Link;
+
 use App\Libraries\Api;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Services\UsersService;
@@ -15,6 +27,210 @@ use Illuminate\Support\Facades\Log;
 
 class UsersController extends Controller
 {
+	public function migrate(Request $request)
+	{
+		$data = $request->json()->all();
+		Team::truncate();
+		User::truncate();
+		Homeadvisor::truncate();
+		Review::truncate();
+		Dialog::truncate();
+		Survey::truncate();
+		Seance::truncate();
+		Text::truncate();
+		Answer::truncate();
+		Message::truncate();
+		Client::truncate();
+		Link::truncate();
+
+		$clients_global = [];
+
+		foreach ($data as $item) {
+			$team = new Team();
+			$team->name = $item['name'];
+			$team->save();
+
+			foreach ($item['users'] as $row) {
+				$user = new User();
+				$user->company_name = $row['company_name'];
+				$user->company_status = $row['company_status'];
+				$user->plans_id = $row['plans_id'];
+				$user->teams_id = $team->id;
+				$user->teams_leader = $row['teams_leader'];
+				$user->type = $row['type'];
+				$user->email = $row['email'];
+				$user->password = $row['password'];
+				$user->firstname = $row['firstname'];
+				$user->lastname = $row['lastname'];
+				$user->phone = $row['phone'];
+				$user->view_phone = $row['phone'];
+				$user->additional_phones = $row['additional_phones'];
+				$user->active = $row['active'];
+				$user->offset = 0;
+				$user->save();
+
+				$link = new Link();
+				$link->users_id = $user->id;
+				$link->code = $row['links']['code'];
+				$link->url = $row['links']['url'];
+				$link->success = $row['links']['success'];
+				$link->save();
+
+				$homeadvisor = new Homeadvisor();
+				$homeadvisor->users_id = $user->id;
+				$homeadvisor->text = $row['homeadvisor']['text'];
+				$homeadvisor->additional_phones = $row['homeadvisor']['additional_phones'];
+				$homeadvisor->rep = $row['homeadvisor']['rep'];
+				$homeadvisor->send_request = $row['homeadvisor']['send_request'];
+				$homeadvisor->active = $row['homeadvisor']['active'];
+				$homeadvisor->save();
+
+				$survey = new Survey();
+				$survey->user_id = $user->id;
+				$survey->text = ! empty($row['surveys']['text']) ? $row['surveys']['text'] : '';
+				$survey->email = $row['surveys']['email'];
+				$survey->subject = $row['surveys']['subject'];
+				$survey->sender = $row['surveys']['sender'];
+				$survey->alerts_emails = $row['surveys']['alerts_emails'];
+				$survey->alerts_stars = $row['surveys']['alerts_stars'];
+				$survey->alerts_often = $row['surveys']['alerts_often'];
+				$survey->save();
+
+				foreach ($user['urls'] as $url) {
+					$url = new Url();
+					$url->user_id = $user->id;
+					$url->name = $url['name'];
+					$url->social_id = $url['social_id'];
+					$url->active = $url['active'];
+					$url->default = $url['default'];
+					$url->save();
+				}
+
+				$list_ids = [];
+				$clients = [];
+				$lists_id = [];
+				$new_list_id = [];
+
+				foreach ($row['lists'] as $old => $list_row) {
+					if (! in_array($old, $lists_id)) {
+						$list = new ContactList();
+						$list->name = $list_row['name'];
+						$list->users_id = $user->id;
+						$list->save();
+
+						$new_list_id[$list_row['lists_id']] = $list->id;
+
+						foreach ($item['clients'] as $client_row) {
+							if ($client_row['lists_id'] == $old && ! in_array($client_row['phones_id'], $clients)) {
+								$client = new Client();
+								$client->team_id = $team->id;
+								$client->firstname = $client_row['firstname'];
+								$client->lastname = $client_row['lastname'];
+								$client->phone = $client_row['phone'];
+								$client->view_phone = $client_row['view_phone'];
+								$client->email = $client_row['email'];
+								$client->source =  $client_row['source'];
+								$client->save();
+
+								$clients[] = $client_row['phones_id'];
+								$clients_global[$client_row['phones_id']] = $client->id;
+							}
+						}
+						$lists_id[] = $old;
+					}
+				}
+
+				foreach ($user['messages'] as $message) {
+					$temp = [];
+					foreach ($message['lists_id'] as $ids_lists) {
+						$temp[] = $new_list_id[$ids_lists];
+					}
+
+					$message = new Message();
+					$message->user_id = $user->id;
+					$mrssage->lists_id = implode(',', $temp);
+					$message->text = $message['text'];
+					$message->file = $message['file'];
+					$message->schedule = $message['schedule'];
+					$message->switch = $message['switch'];
+					$message->x_day = $message['x_day'];
+					$message->date = $message['date'];
+					$message->finish_date = $message['finish_date'];
+					$message->token = $message['token'];
+					$message->active = $message['active'];
+					$message->save();
+
+					$date = Carbon::now();
+					$date->timestamp = $message->date;
+
+					$text = new Text();
+					$text->message_id = $message->id;
+					$text->phones = 0;
+					$text->finish = 1;
+					$text->message = '';
+					$text->send_at = $date;
+					$text->save();
+				}
+
+				foreach ($user['dialogs'] as $dialogs_row) {
+					$dialog = new Dialog();
+					$dialog->users_id = $user->id;
+					$dialog->clients_id = $clients_global[$dialogs_row['clients_id']];
+					$dialog->text = $dialogs_row['text'];
+					$dialog->my = $dialogs_row['my'];
+					$dialog->new = $dialogs_row['new'];
+					$dialog->status = $dialogs_row['status'];
+
+					$dialog->save();
+				}
+			}
+
+			foreach ($item['clients'] as $client_row) {
+				$client = new Client();
+				$client->team_id = $team->id;
+				$client->firstname = $client_row['firstname'];
+				$client->lastname = $client_row['lastname'];
+				$client->phone = $client_row['phone'];
+				$client->view_phone = $client_row['view_phone'];
+				$client->email = $client_row['email'];
+				$client->source = $client_row['source'];
+				$client->save();
+
+				foreach ($client_row['seances'] as $seances_row) {
+					$review = new Review();
+					$review->user_id = $user->id;
+					$review->survey_id = $survey->id;
+					$review->save();
+
+					$seance = new Seance();
+					$seance->review_id = $review->id;
+					$seance->client_id = $client->id;
+					$seance->url_id = 0;
+					$seance->code = $seances_row['code'];
+					$seance->url = $seances_row['url'];
+					$seance->date = $seances_row['date'];
+					$seance->completed = $seances_row['completed'];
+					$seance->show = $seances_row['show'];
+					$seance->type = $seances_row['type'];
+					$seance->finish = $seances_row['finish'];
+					$seance->success = $seances_row['success'];
+					$seance->message = $seances_row['message'];
+					$seance->alert = $seances_row['alert'];
+
+					$seance->save();
+
+					foreach ($seances_row['answers'] as $answer_row) {
+						$answer = new Answer();
+						$answer->seance_id = $seance->id;
+						$answer->question_id = $answer_row['question_id'];
+						$answer->value = $answer_row['value'];
+						$answer->save();
+					}
+				}
+			}
+		}
+	}
+
     public function info($id)
     {
     	return User::find($id);
