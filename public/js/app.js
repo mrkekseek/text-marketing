1eaa8c8d6bf865a1d65257975e7d2fb8a9299905
Ievgenii Domanskyy <jb@div-art.com>
2018-01-05 10:45:20 +0200

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('app', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'ngFileUpload', 'ngAnimate']);
+    angular.module('app', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'ngAnimate']);
 })();
 
 ;
