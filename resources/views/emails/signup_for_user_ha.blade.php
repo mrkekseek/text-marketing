8cfeef75b4912be937afc1664a9c43636146f57d
Ievgenii Domanskyi <id@div-art.com>
2018-02-23 16:34:06 +0200

    Changed text for signup email

--- resources/views/emails/signup_for_user_ha.blade.php
+++ resources/views/emails/signup_for_user_ha.blade.php
@@ -5,8 +5,8 @@ Hi {{ $user->firstname }},<br />
 Thanks for signing up with us!<br />
 Right now we are sending your account info over to HomeAdvisor to get you connected. Normally it takes a day or so for them - once they do we will put in our template text and send you a test text for your approval. We won't make you live until then.<br />
 <p>We will be in touch soon, thanks!</p>
-<p>We will send a test text to your cell before we go live.</p>
-<p>To see a short video explanation, <a href="https://www.contractortexter.com/video">CLICK HERE</a></p>
+<p>Log into you account anytime here: <a href="https://app.contractortexter.com">app.contractortexter.com</a></p>
+<p>To see a short video explanation, <a href="https://contractortexter.com/video">CLICK HERE</a></p>
 Uri<br />
 CEO | Cofounder <br />
 {{ config('mail.from.address') }}
