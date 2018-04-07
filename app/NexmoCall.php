f64a1f815517ae9c2ab22fdbf6b9cc3624ee4f06
Ievgenii Domanskyi <id@div-art.com>
2018-04-07 15:27:07 +0300

    asdasd

--- app/NexmoCall.php
+++ app/NexmoCall.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class NexmoCalls extends Model
+class NexmoCall extends Model
 {
-    protected $guarded = [];
+    //protected $guarded = [];
 }
