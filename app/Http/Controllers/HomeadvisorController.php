b374e36ad9e644a654e893a69987f5b77de54668
Ievgenii Domanskyi <id@div-art.com>
2018-04-07 13:37:58 +0300

    put print_r request on webhook in logger file

--- app/Http/Controllers/HomeadvisorController.php
+++ app/Http/Controllers/HomeadvisorController.php
@@ -807,6 +807,7 @@ class HomeadvisorController extends Controller
 	
 	public function answer(Request $request)
     {
+		file_put_contents('logs/logger.txt', print_r($request, true).PHP_EOL, FILE_APPEND | LOCK_EX);
 		$method = $_SERVER['REQUEST_METHOD'];
 
 		switch ($method) {
