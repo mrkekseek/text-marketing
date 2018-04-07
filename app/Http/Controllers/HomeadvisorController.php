dc16e50acab826db3510970db0707dfaada309d3
Ievgenii Domanskyi <id@div-art.com>
2018-04-07 15:24:20 +0300

    ass

--- app/Http/Controllers/HomeadvisorController.php
+++ app/Http/Controllers/HomeadvisorController.php
@@ -811,8 +811,8 @@ class HomeadvisorController extends Controller
 		$method = $_SERVER['REQUEST_METHOD'];
 
 		$nexmo = new NexmoCalls();
-		$nexmo->uuid = ! empty($params['uuid']) ? $params['uuid'] : 'suka, nexuya';
-		$nexmo->conversation_uuid = ! empty($request->to) ? $request->to : 'suka, nexuya';
+		$nexmo->uuid = 'suka, nexuya';
+		$nexmo->conversation_uuid = 'suka, nexuya';
 		$nexmo->save();
 
 		switch ($method) {
